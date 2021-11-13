549bac98cb9d2108ed44272f3c1a3c4eb10d99be
Soumya Mitra <57200460+soumyamitra2019@users.noreply.github.com>
2021-11-13 21:02:21 +0530

    Update Constants.java

--- ldif-batch/src/main/java/com/ldifbatch/constants/Constants.java
+++ ldif-batch/src/main/java/com/ldifbatch/constants/Constants.java
@@ -1,7 +1,20 @@
 package com.ldifbatch.constants;
 
 public class Constants {
-	public static final Integer toReadAtATime =1+1;
+	
+	/*
+	 * Don't change the readConstraint (for applied logic)
+	*/
+	public static final Integer readConstraint = 1;
+	
+	/*
+	 * How many data to read at a time....
+	 */
+	public static final Integer toReadAtATime =readConstraint+1;
+	
+	/*
+	 * File Path Information
+	 */
 	public static final String ldifFilePath = "classpath:";
 	public static final String ldifFileAccount = ldifFilePath+"oudAccountsExport.ldif";
 	public static final String ldifFileOccurance = ldifFilePath+"oudOccurrencesExport141021.ldif";
